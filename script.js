a2f09afa4dd5cb74a2eca4656c841d93a7cc57d7
Shatun26 <emagrof20@gmail.com>
2021-05-01 23:31:44 +0300

    Change style

--- script.js
+++ script.js
@@ -3,23 +3,23 @@ let boxParent = document.querySelector('.boxParent');
 let pText = document.querySelector('.pText');
 
 document.querySelector('.boxParent').addEventListener('mousemove', (e) => {
-  let coordX = relativeCoords(e).x - 325;
-  let coordY = relativeCoords(e).y - 325;
+  let coordX = relativeCoords(e).x - 500;
+  let coordY = relativeCoords(e).y - 250;
 
   pText.innerText = `X = ${coordX} || Y = ${coordY}`;
-  box.style.transform = `rotateY(${coordX / 20}deg) rotateX(${-coordY / 20}deg)`;
-  // document.querySelector('.subBox').style.boxShadow = `${-coordX/40}px ${-coordY/40}px 8px black`;
-  document.querySelector('.box').style.boxShadow = `${-coordX / 40}px ${
+  box.style.transform = `rotateY(${coordX / 40}deg) rotateX(${
     -coordY / 40
-  }px 8px black`;
-
+  }deg)`;
   [...document.querySelectorAll('.subBox')].forEach((box) => {
-    box.style.boxShadow = `${-coordX / 40}px ${-coordY / 40}px 8px #57213c`;
+    box.style.boxShadow = `${-coordX / 20}px ${-coordY / 20}px 10px #00263144,
+    0px 0px 10px #00263144`;
   });
 });
+
 document.querySelector('.boxParent').addEventListener('mouseover', (e) => {
   box.classList.remove('active');
 });
+
 document.querySelector('.boxParent').addEventListener('mouseout', (e) => {
   box.classList.add('active');
 });
